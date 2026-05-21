7c8a7a09a1426965162679b102b4569d0b306661
dennismzanetti <dennismzanetti@gmail.com>
2026-05-21 10:38:49 -0400

    refactor: wrap syncDateInput in requestAnimationFrame for tab-cycle safety

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-Ly8gLS0tLSBGaXJlYmFzZSBpbml0IC0tLS0tCmNvbnN0IGZpcmViYXNlQ29uZmlnID0gewogIGFwaUtleTogIkFJemFTeUQ3NUVR
+// ---- Firebase init -----
+const firebaseConfig = {
+  apiKey: "AIzaSyD75EQyz7w9ZYuK8iDewQDzI5Z2RUzMk1k",
+  authDomain: "fibrosymptomtracker.firebaseapp.com",
+  projectId: "fibrosymptomtracker",
+  storageBucket: "fibrosymptomtracker.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abc123"
+};
