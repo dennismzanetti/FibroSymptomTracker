f518700d3d8dd56915346a32181b572aef84156e
dennismzanetti <dennismzanetti@gmail.com>
2026-06-22 12:55:18 -0400

    refactor: move firebase-init.js to js/core/ (Step 1 of folder restructure)

---
+++ js/core/firebase-init.js
@@ -0,0 +1,18 @@
+// firebase-init.js
+// NOTE: Firebase is initialised in app.js which owns the active project config.
+// This file is retained for reference but is not loaded by index.html.
+// The export keyword has been removed to prevent syntax errors if this file
+// is ever loaded as a plain script in future.
+
+// If you wish to migrate to this config, copy the credentials below into
+// app.js and remove the old firebaseConfig block there.
+
+// Alternate project config (currently unused):
+// const altFirebaseConfig = {
+//   apiKey: "AIzaSyD7bnVAfclKhMjfqX3VQ1FP-EKyQIMV5Kc",
+//   authDomain: "fibrosymptomtracker.firebaseapp.com",
+//   projectId: "fibrosymptomtracker",
+//   storageBucket: "fibrosymptomtracker.appspot.com",
+//   messagingSenderId: "399399538093",
+//   appId: "1:399399538093:web:8a0d4d90e0f81a5e8bb22a"
+// };
